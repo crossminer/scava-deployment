4eefe05cca6ba07041c588f15f7acb848c54e650
Marcio Mateus <marcio.mateus@unparallel.pt>
2018-09-07 00:08:51 +0100

    Added scava-metric (based on debian)

---
+++ dashboard/importer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd scava-metrics
+./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics
+
+kidash.py -e http://bitergia:bitergia@elasticsearch:9200 --import panels/scava-project.json
