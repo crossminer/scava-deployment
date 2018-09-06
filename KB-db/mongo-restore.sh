b76f55a0dc3d55c77a221b566c2d65e9112a904a
md2manoppello <juri.di.rocco@gmail.com>
2018-09-06 18:08:16 +0200

    Changed CROSSMINER KB dump

--- KB-db/mongo-restore.sh
+++ KB-db/mongo-restore.sh
@@ -22,7 +22,7 @@ done
 
 # Restore from dump
 
-mongorestore --drop --gzip --archive=CROSSMINER.20180808.gz
+mongorestore --drop --gzip --archive=CROSSMINER.20180906.gz
 
 # Keep container running
 tail -f /dev/null
