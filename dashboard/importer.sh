a0bbdde1e31ae159ee6daa44f81cfd2f672ca0bb
Marcio Mateus <marcio.mateus@unparallel.pt>
2018-09-07 04:48:13 +0100

    Corrections to data importer

--- dashboard/importer.sh
+++ dashboard/importer.sh
@@ -3,4 +3,4 @@
 cd scava-metrics
 ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics
 
-kidash.py -e http://bitergia:bitergia@elasticsearch:9200 --import panels/scava-project.json
+kidash -e https://admin:admin@elasticsearch:9200 --import panels/scava-project.json
