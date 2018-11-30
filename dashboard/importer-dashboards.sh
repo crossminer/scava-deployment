c4a7f3cfb9015c60715a5cbcf87b62eb9ed8223d
Valerio Cosentino <valcos@bitergia.com>
2018-11-30 19:12:52 +0100

    [dashboard] Split importer
    This code splits the commands executed by the importer in two
    files, one imports the dashboards while the other one takes
    care of uploading the data to ElasticSearch. Furthermore, this
    code updates also the kibiter port, which passed from 5601 to 80.

---
+++ dashboard/importer-dashboards.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd scava-metrics
+# Import knowledge base dashboards
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/scava-project.json
+
+cd ../sentiment
+# Import sentiment dashboards
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/perceval_bugs_emotion_fear.json
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/perceval_bugs_emotions.json
+
+cd ../dependencies
+# Import dependencies dashboards
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/dependencies.json
