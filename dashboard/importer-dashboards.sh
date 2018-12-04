36e31b829ee89aa066abbbf4bed69f37724f11f3
tdegueul <degueule@cwi.nl>
2018-12-04 11:10:18 +0100

    Fixing kibiter port in importer-dashboards.sh

--- dashboard/importer-dashboards.sh
+++ dashboard/importer-dashboards.sh
@@ -2,13 +2,13 @@
 
 cd scava-metrics
 # Import knowledge base dashboards
-kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/scava-project.json
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:5601 --import panels/scava-project.json
 
 cd ../sentiment
 # Import sentiment dashboards
-kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/perceval_bugs_emotion_fear.json
-kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/perceval_bugs_emotions.json
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:5601 --import panels/perceval_bugs_emotion_fear.json
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:5601 --import panels/perceval_bugs_emotions.json
 
 cd ../dependencies
 # Import dependencies dashboards
-kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:80 --import panels/dependencies.json
+kidash -e https://admin:admin@elasticsearch:9200 --kibana-url http://admin:admin@kibiter:5601 --import panels/dependencies.json
