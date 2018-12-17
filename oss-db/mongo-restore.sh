7554c56cf93c14eeab31120af8c062dc58f0ce29
Marcio Mateus <marcio.mateus@unparallel.pt>
2018-12-17 21:29:31 +0000

    Reduction of messages in console

--- oss-db/mongo-restore.sh
+++ oss-db/mongo-restore.sh
@@ -6,7 +6,10 @@ rm /var/log/mongodb.log
 touch /var/log/mongodb.log
 chmod 777 /var/log/mongodb.log
 
-/entrypoint.sh mongod --repair --port 8001
+echo "Starting DB repair"
+/entrypoint.sh mongod --repair --logpath /var/log/mongodb.log --logappend --port 8001
+
+echo "Starting restore session"
 
 # Start mongodb with logging
 # --logpath    Without this mongod will output all log information to the standard output.
@@ -27,26 +30,42 @@ done
 # Restore from dump
 
 
+echo "Restoring (0/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=JDBM.2018.11.16.gz &&
+echo "Restoring (1/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=JDBM3.2018.11.16.gz &&
+echo "Restoring (2/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=Jest.2018.11.16.gz &&
+echo "Restoring (3/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=SVNKIT.2018.11.16.gz &&
+echo "Restoring (4/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=elasticsearch-java-client.2018.11.16.gz &&
+echo "Restoring (5/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=elasticsearchjava.2018.11.16.gz &&
+echo "Restoring (6/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=elasticsearchjavaclient.2018.11.16.gz &&
+echo "Restoring (7/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=emf.2018.11.16.gz &&
+echo "Restoring (8/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=gef.2018.11.16.gz &&
+echo "Restoring (9/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=netty.2018.11.16.gz &&
+echo "Restoring (10/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=scava-analysis.2018.11.16.gz &&
+echo "Restoring (11/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=scava.2018.11.16.gz &&
+echo "Restoring (12/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=spring-elasticsearch.2018.11.16.gz &&
+echo "Restoring (13/14)" &&
+mongorestore  --port 8000 --gzip --quiet --archive=springelasticsearch.2018.11.16.gz &&
+echo "Restoring (14/14)" &&
 
-mongorestore  --port 8000 --gzip --archive=JDBM.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=JDBM3.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=Jest.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=SVNKIT.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=elasticsearch-java-client.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=elasticsearchjava.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=elasticsearchjavaclient.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=emf.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=gef.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=netty.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=scava-analysis.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=scava.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=spring-elasticsearch.2018.11.16.gz &&
-mongorestore  --port 8000 --gzip --archive=springelasticsearch.2018.11.16.gz &&
+/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend --port 8000 --shutdown &&
+echo "Restore session closed" &&
 
 
-/entrypoint.sh mongod --port 8000 --shutdown &&
-
-/entrypoint.sh mongod &&
+echo "Starting listening session on port 27017" &&
+/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &&
 
 # Keep container running
 tail -f /dev/null
