ff4d693cec0aca6d957cc364f4bc1cca00bc5a34
ambpro <amin.boudeffa@softeam.fr>
2018-12-17 15:11:28 +0100

    Add Api-Gateway script config

---
+++ web-admin/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Starting application..."
+echo "API_GATEWAY_URL = ${API_GATEWAY_VAR}"
+json_file="/usr/share/nginx/html/assets/config/externalConfig.json"
+
+if [ -n "${API_GATEWAY_VAR}" ]
+then
+        echo "{" > "${json_file}"
+        echo "  \"SERVICE_URL\" :\"${API_GATEWAY_VAR}\"" >>"${json_file}"
+        echo "{" >>"${json_file}"
+
+	echo "------------- cat ${json_file} ---------------------"
+	cat "${json_file}"
+fi
