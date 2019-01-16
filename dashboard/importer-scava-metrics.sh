f65436d26ab2634f7fc727309e3420e0b8e875c4
Valerio Cosentino <valcos@bitergia.com>
2019-01-16 11:39:43 +0100

    [dashboard] Enable debug using env variable
    This code allows to enable/disable the debug mode using an env variable defined
    through the docker-compose-build.

--- dashboard/importer-scava-metrics.sh
+++ dashboard/importer-scava-metrics.sh
@@ -2,4 +2,9 @@
 
 # Import all the Scava metrics to Elasticsearch to be used in Kibana
 cd scava-metrics
-./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics
+
+if [[ $DASHDEBUG -eq 1 ]]; then
+    ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics;
+else
+    ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics;
+fi
