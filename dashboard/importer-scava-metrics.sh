9a4ba821dc893d724f39b0d1e06f7dcf0e3362ba
Valerio Cosentino <valcos@bitergia.com>
2019-05-14 13:57:56 +0200

    [dashboard] Include dependency data to elasticsearch
    This code modifies the importer-scava-metrics.sh to extract the dependency
    data from the raw endpoint of the SCAVA API, process and finally store it to
    elasticsearch.

--- dashboard/importer-scava-metrics.sh
+++ dashboard/importer-scava-metrics.sh
@@ -6,7 +6,9 @@ cd scava-metrics
 if [[ $DASHDEBUG -eq 1 ]]; then
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics --category metric;
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-factoids --category factoid;
+    ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-deps --category dependency;
 else
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics --category metric;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-factoids --category factoid;
+    ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-deps --category dependency;
 fi
