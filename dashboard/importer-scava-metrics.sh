0451b9da7c51f91f60bb424c14d8dca447cddb31
Valerio Cosentino <valcos@bitergia.com>
2019-06-15 21:06:08 +0200

    [dashboard-importer] Fix recommendation url

--- dashboard/importer-scava-metrics.sh
+++ dashboard/importer-scava-metrics.sh
@@ -10,7 +10,7 @@ if [[ $DASHDEBUG -eq 1 ]]; then
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-conf-deps --category conf-dependency --bulk-size $BULKSIZE;
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-dep-versions --category version-dependency --bulk-size $BULKSIZE;
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-users --category user --bulk-size $BULKSIZE;
-    ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-recommendations --category recommendation --bulk-size $BULKSIZE;
+    ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 --recommendation-url http://kb-service:8080/api -i scava-recommendations --category recommendation --bulk-size $BULKSIZE;
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-conf-smells --category conf-smell --bulk-size $BULKSIZE;
 else
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics --category metric --bulk-size $BULKSIZE;
@@ -19,7 +19,7 @@ else
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-conf-deps --category conf-dependency --bulk-size $BULKSIZE;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-dep-versions --category version-dependency --bulk-size $BULKSIZE;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-users --category user --bulk-size $BULKSIZE;
-    ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-recommendations --category recommendation --bulk-size $BULKSIZE;
+    ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 --recommendation-url http://kb-service:8080/api -i scava-recommendations --category recommendation --bulk-size $BULKSIZE;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-conf-smells --category conf-smell --bulk-size $BULKSIZE;
 fi
 
