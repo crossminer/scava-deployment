c34a587096cc8088434c83e247e9c2f47fac30b5
Valerio Cosentino <valcos@bitergia.com>
2019-06-16 13:48:19 +0200

    [dashboard-importer] Import project relations data to ES
    This code allows to import project relations to ES, thus enabling
    the creation of visualizations.

--- dashboard/importer-scava-metrics.sh
+++ dashboard/importer-scava-metrics.sh
@@ -12,6 +12,7 @@ if [[ $DASHDEBUG -eq 1 ]]; then
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-users --category user --bulk-size $BULKSIZE;
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 --recommendation-url http://kb-service:8080/api -i scava-recommendations --category recommendation --bulk-size $BULKSIZE;
     ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-conf-smells --category conf-smell --bulk-size $BULKSIZE;
+    ./scava2es.py -g -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-project-relations --category project-relation --bulk-size $BULKSIZE;
 else
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics --category metric --bulk-size $BULKSIZE;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-factoids --category factoid --bulk-size $BULKSIZE;
@@ -21,8 +22,9 @@ else
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-users --category user --bulk-size $BULKSIZE;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 --recommendation-url http://kb-service:8080/api -i scava-recommendations --category recommendation --bulk-size $BULKSIZE;
     ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-conf-smells --category conf-smell --bulk-size $BULKSIZE;
+    ./scava2es.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-project-relations --category project-relation --bulk-size $BULKSIZE;
 fi
 
 if [[ $SYNC_ES_SCAVA -eq 1 ]]; then
-    ./synccontent.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics scava-factoids scava-deps;
+    ./synccontent.py -u http://oss-app:8182 -e https://admin:admin@elasticsearch:9200 -i scava-metrics scava-factoids scava-dev-deps scava-conf-deps scava-dep-versions scava-recommendations scava-conf-smells scava-project-relations;
 fi
